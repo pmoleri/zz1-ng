71f178330913d6b0234afe51ac3120ffaa5568a4
pmoleri <pmoleri@gmail.com>
2023-02-22 13:59:34 -0300

    untitled-app (55) initial version

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MasterViewComponent } from './master-view/master-view.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MasterViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    HammerModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
